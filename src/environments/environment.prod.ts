b07c86970f4f93647f2689da35a98d62bb9a41c2
KwameAmoahh <kwame.amoah498@gmail.com>
2025-09-11 21:25:38 +0200

    Moved environment variables to github secrets, now accessed from environment.prod.ts. Stopped tracking environment.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://campus-study-budy-fwhpaahfach9g8bw.canadacentral-01.azurewebsites.net',
+  supabaseUrl: '__AUTH_URL__',
+  supabaseKey: '__AUTH_KEY__',
+  API_KEY_ADMIN: '__API_KEY__'
+};
