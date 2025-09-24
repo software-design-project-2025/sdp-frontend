0f17f9e5d2af3eca6ed14f27bd94eb96e416ee40
KwameAmoahh <kwame.amoah498@gmail.com>
2025-09-24 19:26:26 +0200

    Imported correct environments file

--- src/app/services/academic.service.ts
+++ src/app/services/academic.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
-//import {environment} from '../../environments/environment';
+import {environment} from '../../environments/environment.prod';
 
 @Injectable({
     providedIn: 'root' // Makes the service a singleton
@@ -9,11 +9,11 @@ import { Observable } from 'rxjs';
 export class AcademicApiService {
     constructor(private http: HttpClient) { }
 
-    private url = "https://campus-study-budy-fwhpaahfach9g8bw.canadacentral-01.azurewebsites.net";
+    private url = environment.apiBaseUrl;
 
     private getHeaders(): HttpHeaders {
         return new HttpHeaders({
-            'Authorization': `Bearer -LYaFN3XV-G9vVIEp0kwF9TlB3mcFyZFfZ9yWmV5gd4`, //needs fixing
+            'Authorization': `Bearer ${environment.API_KEY_ADMIN}`, //needs fixing
             'Content-Type': 'application/json'
         });
     }
