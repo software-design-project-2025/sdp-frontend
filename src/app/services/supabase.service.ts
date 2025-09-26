ee452eed598608b879d569cda17879f5501df7a3
Monare Selahle <165192023+monare-code@users.noreply.github.com>
2025-09-26 16:56:46 +0200

    Update supabase.service.ts url (#32)

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -5,7 +5,7 @@ import { environment } from '../../environments/environment.prod'
 
 
 export const supabase = createClient(
-  environment.apiBaseUrl,
+  environment.supabaseUrl,
   environment.supabaseKey
 );
 
