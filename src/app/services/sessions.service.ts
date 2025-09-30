a8289e15cc719fea3742b4df0c2728593ad2eb94
Sharlene <sharlenemoetjie18@gmail.com>
2025-09-30 03:25:56 +0200

    environment to environment.prod

--- src/app/services/sessions.service.ts
+++ src/app/services/sessions.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Session } from '../models/session.model';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
