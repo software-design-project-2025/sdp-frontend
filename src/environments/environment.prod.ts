72d7993a8f4e819131763a689d71be7e72271761
Sharlene <sharlenemoetjie18@gmail.com>
2025-10-22 12:00:26 +0200

    fix study hours and num of sessions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://newcsb-bpfwapg6dsf6exev.canadacentral-01.azurewebsites.net',
-  supabaseUrl: '__AUTH_URL__',
-  supabaseKey: '__AUTH_KEY__',
-  API_KEY_ADMIN: '__API_KEY__'
+  supabaseUrl: '_AUTH_URL_',
+  supabaseKey: '_AUTH_KEY_',
+  API_KEY_ADMIN: '_API_KEY_'
 };
